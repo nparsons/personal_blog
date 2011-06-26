d68dbb19f4405cec74e0ac079ed8b3a71d9661e6
Nicholas Parsons <parsons.nicholas1@gmail.com>
2011-06-26 07:10:03 -0400

    Added model code for blog

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module PersonalBlog
   class Application < Rails::Application
+    # Activate observers that should always be running
+    config.active_record.observers = :comment_observer
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
